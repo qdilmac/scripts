102fbb8fe2fea745c0ec3bed4a3416af7e73c88f
qdilmac <shadeofficial7@outlook.com>
2023-08-02 13:51:03 +0300

    opencv day 2

--- opencv7.py
+++ opencv7.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+
+# Color Filtering!
+
+import numpy as np
+import cv2
+
+image = cv2.imread("images/tennis.jpeg")
+cv2.imshow("Normal",image)
+
+# first we convert RGB to HSV
+hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+cv2.imshow("HSV",hsv)
+
+# tennis ball's color values / lower and upper values / find values that works, there is no specific value
+yellowLower = (27.5, 10, 10) # for the image that i used these values are almost perfect
+yellowUpper = (50, 255, 255)
+
+# mask definition
+mask = cv2.inRange(hsv,yellowLower,yellowUpper)
+cv2.imshow("Mask",mask)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
